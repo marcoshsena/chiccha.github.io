737e53b8b94ecb7f0aafbb1ac7408faca076f1e3
marcoshsena <168047441+marcoshsena@users.noreply.github.com>
2025-12-02 09:14:50 -0300

    Create produtos.js

---
+++ .js/produtos.js
@@ -0,0 +1,22 @@
+import { db } from "./firebase.js";
+import { doc, getDoc, setDoc } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+async function verificarProdutos() {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(async card => {
+        const produto = card.getAttribute("data-name");
+        const ref = doc(db, "produtos", produto);
+        const snap = await getDoc(ref);
+
+        if (snap.exists() && snap.data().comprado === true) {
+            const botao = card.querySelector(".btn");
+            botao.innerText = "Indisponível";
+            botao.style.background = "#999";
+            botao.style.pointerEvents = "none";
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", verificarProdutos);

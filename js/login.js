00a13ddc3bc75b8155dcb9ad6986bd78e85aaaa8
marcoshsena <168047441+marcoshsena@users.noreply.github.com>
2025-12-02 09:32:14 -0300

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,13 @@
+import { auth, signInWithEmailAndPassword } from "./firebase.js";
+
+window.login = async function () {
+  const email = document.getElementById("email").value.trim();
+  const senha = document.getElementById("senha").value.trim();
+
+  try {
+    await signInWithEmailAndPassword(auth, email, senha);
+    window.location.href = "admin.html";
+  } catch (e) {
+    alert("Login inválido!");
+  }
+};

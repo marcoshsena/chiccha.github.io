0edfc65495c85ded54bc3447fed6df57f652c00f
marcoshsena <168047441+marcoshsena@users.noreply.github.com>
2025-12-03 10:16:32 -0300

    Update produtos.js

--- js/produtos.js
+++ js/produtos.js
@@ -1,20 +1,27 @@
+// js/produto.js
 import { db } from "./firebase.js";
-import { doc, getDoc, setDoc } 
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { doc, getDoc } 
+from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 async function verificarProdutos() {
     const cards = document.querySelectorAll(".card");
 
     cards.forEach(async card => {
         const produto = card.getAttribute("data-name");
+
         const ref = doc(db, "produtos", produto);
         const snap = await getDoc(ref);
 
         if (snap.exists() && snap.data().comprado === true) {
+
             const botao = card.querySelector(".btn");
-            botao.innerText = "Indisponível";
-            botao.style.background = "#999";
+            botao.innerText = "Indisponível ❌";
+            botao.style.background = "#777";
             botao.style.pointerEvents = "none";
+            botao.style.cursor = "not-allowed";
+
+            // opcional: deixar o card cinza
+            card.style.opacity = "0.6";
         }
     });
 }

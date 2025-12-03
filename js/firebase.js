67d3b46ef981c419b153cc5ba1ecefb23a7a6ed8
marcoshsena <168047441+marcoshsena@users.noreply.github.com>
2025-12-03 14:20:15 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -17,4 +17,3 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const onAuthStateChanged = onAuthStateChanged

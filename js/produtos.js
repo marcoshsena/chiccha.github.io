c3e419cfcdb716d6993191e5fdd7518e2f21166f
marcoshsena <168047441+marcoshsena@users.noreply.github.com>
2025-12-04 13:29:10 -0300

    Update filters produtos.js

--- js/produtos.js
+++ js/produtos.js
@@ -5,25 +5,32 @@ import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-
 async function verificarProdutos() {
     const cards = document.querySelectorAll(".card");
 
-    cards.forEach(async card => {
+    for (const card of cards) {
         const produtoId = card.getAttribute("data-id");
-        if (!produtoId) return;
+        if (!produtoId) continue;
 
         const ref = doc(db, "produtos", produtoId);
+
         try {
             const snap = await getDoc(ref);
-            if (snap.exists() && snap.data().comprado === true) {
+
+            if (snap.exists() && snap.data().comprado) {
+
                 const botao = card.querySelector(".btn");
                 botao.innerText = "Indisponível ❌";
                 botao.style.background = "#777";
                 botao.style.pointerEvents = "none";
                 botao.style.cursor = "not-allowed";
+
                 card.style.opacity = "0.6";
+
+                // MARCA COMO INDISPONÍVEL
+                card.classList.add("produto_indisponivel");
             }
         } catch (err) {
             console.error("Erro ao verificar produto:", produtoId, err);
         }
-    });
+    }
 }
 
 document.addEventListener("DOMContentLoaded", verificarProdutos);
